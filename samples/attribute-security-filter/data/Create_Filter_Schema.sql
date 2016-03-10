b14f131e6e243ecbf63dbdfaa446e97c70d746ce
unknown <cars3788@carsten.esri.com>
2016-03-09 20:44:50 -0800

    Updates filter schema to avoid sub select in where clause.

---
+++ samples/attribute-security-filter/data/Create_Filter_Schema.sql
@@ -0,0 +1,65 @@
+DROP TABLE Filter_User_Group
+GO
+
+DROP TABLE Filter_Group_Agency
+GO
+
+DROP TABLE Filter_User
+GO
+
+DROP TABLE Filter_Group
+GO
+
+DROP TABLE Agency
+GO
+
+CREATE TABLE Filter_Group (
+	Group_ID INT,
+	Group_Name NVARCHAR(50)
+	CONSTRAINT PK_Filter_Group PRIMARY KEY NONCLUSTERED (Group_ID),
+	CONSTRAINT UK_Filter_Group_1 UNIQUE (Group_Name),
+)
+GO
+
+CREATE TABLE Filter_User (
+	User_ID INT,
+	User_Name NVARCHAR(50),
+	CONSTRAINT PK_Filter_User PRIMARY KEY (User_ID),
+	CONSTRAINT UK_Filter_User_1 UNIQUE (User_Name)
+	
+)
+GO
+
+CREATE TABLE Filter_User_Group (
+	User_ID INT,
+	Group_ID INT
+	CONSTRAINT PK_Filter_User_Group PRIMARY KEY (User_ID, Group_ID),
+	CONSTRAINT FK_Filter_User_Group_Filter_User FOREIGN KEY (User_ID) 
+	REFERENCES Filter_User (User_ID),
+	CONSTRAINT FK_Filter_User_Group_Filter_Group FOREIGN KEY (Group_ID) 
+    REFERENCES Filter_Group (Group_ID)
+)
+GO
+
+CREATE TABLE Agency (
+	Agency_ID INT,
+	Agency_Name NVARCHAR(50)
+	CONSTRAINT PK_Request_Type PRIMARY KEY (Agency_ID),
+	CONSTRAINT UK_Request_Type_1 UNIQUE (Agency_Name)
+)
+GO
+
+CREATE TABLE Filter_Group_Agency (
+	Group_ID INT,
+	Agency_ID INT,
+	CONSTRAINT PK_Filter_Group_Agency PRIMARY KEY (Group_ID, Agency_ID),
+	CONSTRAINT FK_Filter_Group_Agency_Filter_Group FOREIGN KEY (Group_ID) 
+    REFERENCES Filter_Group (Group_ID),
+	CONSTRAINT FK_Filter_Group_Agency_Agency FOREIGN KEY (Agency_ID) 
+	REFERENCES Agency (Agency_ID)
+)
+GO
+
+CREATE INDEX I_Incident_1 ON Incident (Responsible_Agency)
+GO
+
